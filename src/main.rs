f3a28593707816cbe3f18beae03080c1f920b7ab
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-09 16:58:19 -0800

    Hello world (#1)
    * runnining through some initial stuff
    
    * pushing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod chapters;
+
 fn main() {
-    println!("Hello, world!");
+    chapter_one();
+    chapter_two();
+    chapter_three();
+}
+
+fn chapter_one() {
+    use crate::chapters::ch1::*;
+    println!("{:-^5} Chapter 1 {:-^5}", "-", "-");
+    hello_world();
+    comments();
+    formatted_print();
+    debug();
+    debug_pretty();
+    display_122_1();
+    display_122_2();
+    testcase_list();
+    foratting_guide();
+}
+
+fn chapter_two() {
+    use crate::chapters::ch2::*;
+    println!("{:-^5} Chapter 2 {:-^5}", "-", "-");
+    literals_and_operators();
+    tuples_lesson();
+    arrays_and_slices();
+}
+
+fn chapter_three() {
+    use crate::chapters::ch3::*;
+    println!("{:-^5} Chapter 3 {:-^5}", "-", "-");
+    structures();
+    enum_nom_nom();
+    enums_use();
+    c_like();
+    testcase_linked_list();
+    constants();
 }
